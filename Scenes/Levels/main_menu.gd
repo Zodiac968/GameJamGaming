0789d73dcebc9ec27ef65f31ebb66668265b9f81
Mohamed Reihan <mohamedreihan968@gmail.com>
2025-10-29 23:30:30 +0530

    Added fadein and fadeout

--- Scenes/Levels/main_menu.gd
+++ Scenes/Levels/main_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-
+@onready var colorRect = $"../ColorRect"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,8 +12,10 @@ func _process(delta: float) -> void:
 
 
 func _on_play_pressed() -> void:
+	colorRect.visible = true
+	colorRect.get_node("AnimationPlayer").play("fade_in")
+	await get_tree().create_timer(1.0).timeout
 	get_tree().change_scene_to_file("res://Scenes/SampleScene.tscn")
-	pass # Replace with function body.
 
 
 func _on_quit_pressed() -> void:
